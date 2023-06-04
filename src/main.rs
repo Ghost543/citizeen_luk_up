56f407d1e4eac6a2bb9d243a9da335e9d269c532
Ghost543 <petermuwanguzi51@gmail.com>
2023-06-04 14:56:33 +0300

    ft- rd_xlsx_files

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::convert::From;
+use std::env;
 use std::error::Error;
+use std::path::{Path, PathBuf};
 
 use calamine::{open_workbook, DataType, Range, Reader, Xlsx, XlsxError};
 use clap::Parser;
@@ -29,14 +31,42 @@ fn reading_file(s: &str) -> Result<Range<DataType>, XlsxError> {
     }
 }
 
-fn main() {
-    let cli = Cli::parse();
-    let nin = cli.nin;
-    println!("n_i_n: {:?}", nin);
-    println!("n_i_n: {:?}", cli.surname.as_deref());
+fn get_file() -> Vec<PathBuf> {
+    let dir = env::current_dir()
+        .unwrap()
+        .join(Path::new("src"))
+        .as_path()
+        .join(Path::new("data"));
 
+    let mut files = Vec::new();
+
+    for i in dir.read_dir().unwrap() {
+        let file = i.unwrap();
+        files.push(file.path());
+    }
+    files.sort();
+    files
+}
+
+fn merging_content_in_files(files: &Vec<PathBuf>) {
+    let mut merged_rows = Vec::new();
+    for file in files {
+        // println!("\n ------------------------------------------------------------------------------------------------------------\n");
+        // println!("{:?}", file);
+        // println!("\n ------------------------------------------------------------------------------------------------------------\n");
+        let range = reading_file(file.to_str().unwrap()).unwrap();
+        // println!("{:?}", range.rows().collect::<Vec<_>>());
+        // println!("\n ------------------------------------------------------------------------------------------------------------\n");
+        for row in range.rows().collect::<Vec<_>>() {
+            merged_rows.push(row);
+        }
+    }
+    println!("{:?}", merged_rows);
+}
+
+fn getting_person(search: &Option<String>) {
     let res = reading_file("src/data/LAKE  VICTORIA SCHOOL.xlsx").unwrap();
-    if let Some(n_i_n) = nin {
+    if let Some(n_i_n) = search {
         let dd = res.rows().find(|r| r[0].get_string().unwrap() == n_i_n);
         println!("{:?}", dd.unwrap());
         println!(
@@ -44,4 +74,13 @@ fn main() {
             res.rows().collect::<Vec<_>>()[1][0].get_string().unwrap()
         );
     }
+}
+
+fn main() {
+    let cli = Cli::parse();
+    let nin = cli.nin;
+    println!("n_i_n: {:?}", nin);
+    println!("n_i_n: {:?}", cli.surname.as_deref());
+
+    getting_person(&nin);
 }
